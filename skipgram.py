944f3b5d7f071de1f05b08842d40d5580ef28682
lalopey <lalopey@gmail.com>
2019-12-06 14:31:29 +0400

    (feat) progress on word2vec features clustering

--- skipgram.py
+++ skipgram.py
@@ -65,6 +65,26 @@ def cosine_similarity(embedding, valid_size=16, valid_window=100, device='cpu'):
 
     return valid_examples, similarities
 
+def cosine_similarity_tweet(tweet_embedding, embedding,  device='cpu'):
+    """ Returns the cosine similarity of validation words with words in the embedding matrix.
+        Here, embedding should be a PyTorch embedding module.
+    """
+
+    embed_vectors = embedding.weight
+
+    magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
+
+    # pick N words from our ranges (0,window) and (1000,1000+window). lower id implies more frequent
+    valid_examples = np.array(random.sample(range(valid_window), valid_size // 2))
+    valid_examples = np.append(valid_examples,
+                               random.sample(range(1000, 1000 + valid_window), valid_size // 2))
+    valid_examples = torch.LongTensor(valid_examples).to(device)
+
+    valid_vectors = embedding(valid_examples)
+    similarities = torch.mm(valid_vectors, embed_vectors.t()) / magnitudes
+
+    return valid_examples, similarities
+
 
 class SkipGramNeg(nn.Module):
     def __init__(self, n_vocab, n_embed, noise_dist=None):
@@ -139,17 +159,13 @@ class NegativeSamplingLoss(nn.Module):
         return -(out_loss + noise_loss).mean()
 
 
-def cosine_similarity_sample(embedding, val, valid_size=16, valid_window=100, device='cpu'):
+def cosine_similarity_sample(embedding, val, device='cpu'):
 
     embed_vectors = embedding.weight
 
     # magnitude of embedding vectors, |b|
     magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
 
-    # pick N words from our ranges (0,window) and (1000,1000+window). lower id implies more frequent
-    valid_examples = np.array(random.sample(range(valid_window), valid_size // 2))
-    valid_examples = np.append(valid_examples,
-                               random.sample(range(1000, 1000 + valid_window), valid_size // 2))
     valid_examples = torch.LongTensor(val).to(device)
 
     valid_vectors = embedding(valid_examples)
